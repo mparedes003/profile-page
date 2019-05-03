975211f6ac92d1575cedf8e1965cf695cbaf26b3
mparedes003 <onnabugeishadev@gmail.com>
2019-05-03 15:38:45 -0700

    Add profile-body content and links

--- src/App.js
+++ src/App.js
@@ -37,6 +37,43 @@ function App() {
         </div>
         <div className="profile-body">
           <h3>About</h3>
+          <p>
+            Hi! I'm Michelle. I'm a Full-Stack Web and Software Developer with a
+            passion for learning new technologies, making new applications,
+            debugging, problem solving and living in the terminal.
+          </p>
+          <h3>Technical Skills</h3>
+          <p>
+            Web Development --> HTML, CSS, React, Django, REST APIs, Node,
+            Express, Javascript, Python, C
+          </p>
+
+          <p>Download Resume</p>
+
+          <h3>Recent Projects</h3>
+          <a
+            className="projects"
+            href="https://cineview.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CineView
+          </a>
+          <br />
+
+          <a
+            className="projects"
+            href="https://your-notes.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Lambda Notes
+          </a>
+
+          <h3>Contact</h3>
+          <a className="projects" href="mailto:michelle.g.paredes@gmail.com">
+            <p>michelle.g.paredes@gmail.com</p>
+          </a>
         </div>
       </div>
     </div>
