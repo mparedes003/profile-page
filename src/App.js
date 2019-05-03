54c1991c607249ead11ea031298b63fee7479cde
mparedes003 <onnabugeishadev@gmail.com>
2019-05-03 15:03:14 -0700

    Add photo, name, and About heading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import profile_pic from "./profile_pic.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-body">
+        <div className="sidebar">
+          <img
+            src={profile_pic}
+            className="photo"
+            alt="Michelle Paredes Photo"
+          />
+          <h3>Michelle Paredes</h3>
+        </div>
+        <div className="profile-body">
+          <h3>About</h3>
+        </div>
+      </div>
     </div>
   );
 }
